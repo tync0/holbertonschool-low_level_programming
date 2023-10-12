c326427b75688f2a0a3dc32730917ac8b4e1748f
tync0 <isgenderlituncay2004@gmail.com>
2023-10-12 07:45:17 -0700

    a

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-#include <string.h>
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
