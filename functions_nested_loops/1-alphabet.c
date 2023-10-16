0b7cc4bbdeb234a18be9d8f22b88222742f3c334
tync0 <isgenderlituncay2004@gmail.com>
2023-10-16 08:56:59 -0700

    dd

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * main [i- check the code
+ * print_alphabet: prints the alphabet
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 void print_alphabet(void)
 {
@@ -13,4 +13,5 @@ void print_alphabet(void)
 	{
 		_putchar(c);
 	}
+	_putchar('\n');
 }
