b9fd1fd5eaaee9dfc19745601ee2e6d6c602090c
tync0 <isgenderlituncay2004@gmail.com>
2023-10-16 08:37:29 -0700

    sa

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+
+	return (0);
+}
