894212efe51f406309c7b1cb9df63ac1d4824086
tync0 <isgenderlituncay2004@gmail.com>
2023-10-16 06:07:24 -0700

    a

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'get number'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = '0' ; c <= '9' ; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
