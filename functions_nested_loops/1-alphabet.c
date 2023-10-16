e9b88fbbc3ee311b70288f4bfb6c6c424f38741b
tync0 <isgenderlituncay2004@gmail.com>
2023-10-16 08:42:25 -0700

    ff

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -5,15 +5,19 @@
  *
  * Return: Always 0.
  */
-int main(void)
+void print_alphabet()
 {
 	char c;
 
 	for (c = 'a' ; c <= 'z' ; c++)
 	{
 		_putchar(c);
-	}
-	_putchar('\n');
+	}	
 
+	return (0);
+}
+int main(void)
+{
+	print_alphabet();
 	return (0);
 }
