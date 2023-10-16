2f7d7401af60878a2e52a24ff8f2cefec1fbb980
tync0 <isgenderlituncay2004@gmail.com>
2023-10-16 06:00:04 -0700

    a

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'get number'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		if (c == 'q' || c == 'e')
+			continue;
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
