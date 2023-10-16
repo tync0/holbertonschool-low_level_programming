a6c57421cdd3787ed5be474eabe0f8a3d70a6029
tync0 <isgenderlituncay2004@gmail.com>
2023-10-16 06:14:52 -0700

    a

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'get number'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z' ; c >= 'a' ; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
