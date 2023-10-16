4bfce59c95847257413b0cff058d8985569401aa
tync0 <isgenderlituncay2004@gmail.com>
2023-10-16 07:08:53 -0700

    a

---
+++ variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'get number'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c, x, y;
+
+	for (c = '0' ; c <= '9' ; c++)
+	{
+		for (x = c + 1 ; x <= '9' ; x++)
+		{
+			for (y = x + 1 ; y <= 0 ; y++)
+			{
+				putchar(c);
+				putchar(x);
+				putchar(y);
+				if (c != '8')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
