366914aae7c8e44c96a85df2b69f5798377c8efe
tync0 <isgenderlituncay2004@gmail.com>
2023-10-16 08:34:48 -0700

    bb

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,14 +1,22 @@
 #include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 
 int main(void)
 {
-	char c[8] = "_putchar";
+	char c[] = "_putchar";
 	int i;
 
 	for (i = 0 ; i < 8 ; i++)
 	{
 		_putchar(c[i]);
 	}
+	_putchar('\n');
 
 	return (0);
 }
