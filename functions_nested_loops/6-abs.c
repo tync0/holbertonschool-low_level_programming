f135d537bb7203f1c90a0b4728ecf76ade19e7e0
tync0 <isgenderlituncay2004@gmail.com>
2023-10-18 02:35:14 -0700

    a

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: the number is checker
+ *
+ * Return: Always 0 (Success)
+ */
+int _abs(int n)
+{
+	return (n < 0 ? -n : n);
+}
