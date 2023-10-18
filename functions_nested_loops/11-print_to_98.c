f2564c4abe4d7f96d0c55d31166517b31798bf2b
tync0 <isgenderlituncay2004@gmail.com>
2023-10-18 07:03:37 -0700

    a

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -4,7 +4,7 @@
 /**
  * print_to_98 - prints all natural numbers from n to 98
  * @n: the number is checker
- * @X: the number is checker
+ * @x: the number is checker
  *
  * Return: Always 0 (Successu)
  */
@@ -28,6 +28,10 @@ void print_to_98(int n)
 		{
 			printf(", %d", x);
 		}
+	}
+	else
+	{
+		printf("%d", 98);
 	}
 	printf("\n");
 }
