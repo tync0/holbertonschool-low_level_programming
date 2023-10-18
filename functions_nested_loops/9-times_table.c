523a6d372f9ec7fb304942e0b7ba1dc1bcec6778
tync0 <isgenderlituncay2004@gmail.com>
2023-10-18 05:09:26 -0700

    a

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -12,31 +12,28 @@ void times_table(void)
 	for (i = 0 ; i < 10 ; i++)
 	{
 		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
 		for (j = 0 ; j < 9 ; j++)
 		{
 			x = x + i;
 			if (x > 9)
 			{
-				_putchar((x / 10) + '0');
-				_putchar((x % 10) + '0');
 				if (j != 8)
 				{
 					_putchar(',');
 					_putchar(' ');
 				}
+				_putchar((x / 10) + '0');
+				_putchar((x % 10) + '0');
 			}
 			else
 			{
-				 _putchar(x + '0');
 				if (j != 8)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 				}
+				 _putchar(x + '0');
 			}
 
 		}
