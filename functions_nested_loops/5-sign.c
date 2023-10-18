8e666b96a4018af19c1661c889384515b9d43772
tync0 <isgenderlituncay2004@gmail.com>
2023-10-18 02:27:39 -0700

    a

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_sign -  prints the sign of a number
+ * @c: the number is checker
+ *
+ * Return: Always 0 (Success)
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
