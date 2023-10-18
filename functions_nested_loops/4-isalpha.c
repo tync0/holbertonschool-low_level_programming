15096f13c8018612e1100580ecb7e3433fff8147
tync0 <isgenderlituncay2004@gmail.com>
2023-10-18 02:20:41 -0700

    a

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character
+ * @c: the number is checker
+ *
+ * Return: Always 0 (Success)
+ */
+int _islower(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
