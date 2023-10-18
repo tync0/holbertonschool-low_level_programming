4345855feea7f8cde62ebb3b347d3f750c492d33
tync0 <isgenderlituncay2004@gmail.com>
2023-10-18 05:49:52 -0700

    a

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: the number is checker
+ *
+ * Return: Always 0 (Success)
+ */
+void print_to_98(int n)
+{
+	int x;
+
+	if (n < 98)
+	{
+		printnumber(n);
+		for (x = n ; x <= 98 ; x++)
+		{
+			if(x != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			printnumber(x);
+
+		}
+	}
+	else if (n > 98)
+	{
+		printnumber(n);
+		for  (x = n ; x >= 98 ; x++)
+		{
+			if (x != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			printnumber(x);
+		}
+	}
+	else
+		printnumber(98);
+}
--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -5,3 +5,21 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+
+void printnumber(int n)
+{
+	if(n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+	if (n / 10)
+	{
+		printnumber(n / 10);
+	}
+	_putchar((n % 10) + '0');
+}
