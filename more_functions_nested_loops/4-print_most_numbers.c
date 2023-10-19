ecb869b282606d609c77bfea9913ac3da2b45a83
tync0 <isgenderlituncay2004@gmail.com>
2023-10-19 06:11:33 -0700

    u

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_most_numbers -  prints the numbers, from 0 to 9
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c = '0' ; c <= '9' ; c++)
+	{
+		_putchar(c);
+		if (c = '2' || c = '4')
+			continue;
+	}
+	_putchar('\n');
+}
