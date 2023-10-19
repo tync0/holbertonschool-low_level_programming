7f8e7421a2b5c953f2e923b4f3c556b0d19b8424
tync0 <isgenderlituncay2004@gmail.com>
2023-10-19 06:09:21 -0700

    tt

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers -  prints the numbers, from 0 to 9
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0' ; c <= '9' ; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
