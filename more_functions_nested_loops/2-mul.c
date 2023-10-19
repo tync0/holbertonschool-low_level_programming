0df17fe23d857f36484efc8caa9f7871419f11e0
tync0 <isgenderlituncay2004@gmail.com>
2023-10-19 06:05:14 -0700

    t

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiplies two integers
+ * @a: checker
+ * @b: checker
+ *
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	return (a+b);
+}
