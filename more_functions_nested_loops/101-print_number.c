151df361b66c66e8fd1135083854dbe2cae8b516
tync0 <isgenderlituncay2004@gmail.com>
2023-10-20 06:02:11 -0700

    a

---
+++ more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_number - prints an integer
+ * @n: checker
+ *
+ * Return: Always 0.
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+		print_number(n / 10);
+	 _putchar(n % 10 + '0');
+	 _putchar('\n');
+}
