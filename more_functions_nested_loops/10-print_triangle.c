718b95fd022672ffbd2f00ac0de1a661b232fcab
tync0 <isgenderlituncay2004@gmail.com>
2023-10-20 05:32:59 -0700

    a

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -9,11 +9,11 @@
  */
 void print_triangle(int size)
 {
-	if (n > 0)
+	if (size > 0)
 	{
 		int i, j;
 
-		for (i = 0 ; i < n ; i++)
+		for (i = 0 ; i < size ; i++)
 		{
 			for (j = 0 ; j < i ; j++)
 				_putchar(' ');
