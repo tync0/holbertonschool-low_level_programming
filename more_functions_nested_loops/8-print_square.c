ab906d611b8e17aa0f33e9c853d853520c3c16e5
tync0 <isgenderlituncay2004@gmail.com>
2023-10-20 04:05:21 -0700

    a

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_square - prints a square
+ * @size: checker
+ *
+ * Return: Always 0.
+ */
+void print_square(int size)
+{
+	if (n > 0)
+	{
+		int i, j;
+
+		for (i = 0 ; i < size ; i++)
+		{
+			for (j = 0 ; j < size ; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else
+		 _putchar('\n');i
+}
