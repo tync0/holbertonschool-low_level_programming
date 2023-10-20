09de2c921149acc922dabf6a5c7730f934dc0728
tync0 <isgenderlituncay2004@gmail.com>
2023-10-20 03:37:49 -0700

    a

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: checker
+ *
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		int i;
+		
+		for (i = 0 ; i < n ; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
