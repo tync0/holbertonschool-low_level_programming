44c438ad433c21aa932289ec3a0492d61a10b4e4
tync0 <isgenderlituncay2004@gmail.com>
2023-10-20 06:13:44 -0700

    a

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -9,12 +9,15 @@
  */
 void print_number(int n)
 {
-	if (n < 0)
+	while (n > 9)
 	{
-		_putchar('-');
-		n = -n;
+		if (n < 0)
+		{
+			_putchar('-');
+			n = -n;
+		}
+		_putchar(n % 10 + '0');
+		n = n / 10;
 	}
-	if (n / 10)
-		print_number(n / 10);
-	 _putchar(n % 10 + '0');
+	_putchar(n % 10 + '0');
 }
