0d07e0ffe53e594918bfa64b574a4e749d672e23
tync0 <isgenderlituncay2004@gmail.com>
2023-10-20 03:30:15 -0700

    a

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -12,11 +12,11 @@ void more_numbers(void)
 
 	for (i = 0 ; i < 10 ; c++)
 	{
-		for (c = 0 ;c <= 14 ; c++)
+		for (c = 0 ; ic <= 14 ; c++)
 		{
 			if (c > 9)
-				_putchar(c / 10);
-			_putchar(c % 10);
+				_putchar(c / 10 + '0');
+			_putchar(c % 10 + '0');
 		}
 		_putchar('\n');
 	}
