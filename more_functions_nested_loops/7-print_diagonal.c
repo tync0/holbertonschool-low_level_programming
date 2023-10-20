a875c1971575e1df1fd74476f2141b012cec85b4
tync0 <isgenderlituncay2004@gmail.com>
2023-10-20 03:58:21 -0700

    a

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: checker
+ *
+ * Return: Always 0.
+ */
+void print_diagonal(int n)
+{
+	if (n > 0)
+	{
+		int i, j;
+
+		for (i = 0 ; i < n ; i++)
+		{
+			for (j = 0 ; j < i ; j++)
+				_putchar(' ');
+			_putchar('\\');
+		}
+	}
+	_putchar('\n');
+}
