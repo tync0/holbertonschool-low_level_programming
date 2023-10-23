c2858ba12a7492b1834046520933c41fd44170c0
tync0 <isgenderlituncay2004@gmail.com>
2023-10-23 07:54:16 -0700

    a

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string - reverses a string
+ * @s: checker
+ *
+ * Return: Always 0.
+ */
+void rev_string(char *s)
+{
+	int i = 0, j = 0, x;
+	char *str = s;
+
+	while (*str != '\0')
+	{
+		str++;
+		i++;
+	}
+	char a[i-1];
+	for (x = i - 1; x >= 0 ; x--)
+	{
+		a[j] = str[x];
+		j++;
+	}
+	s = a;
+}
