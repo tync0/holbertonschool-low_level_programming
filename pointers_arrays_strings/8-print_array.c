cc43b889614fee82cae39a9c2b4c689437bf6975
tync0 <isgenderlituncay2004@gmail.com>
2023-10-23 08:35:03 -0700

    a

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: checker
+ * @n: checker
+ *
+ * Return: Always 0.
+ */
+void print_array(int *a, int n)
+{
+	int *arr = a, i = 0;
+
+	for (; i < n ; i++)
+	{
+		printf("%d", arr[i]);
+		if (i != n - 1)
+			printf(", ");
+	}
+	_putchar('\n');
+}
