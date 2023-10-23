209bccc17a1192cad9735ab5d57ad690e4a92d54
tync0 <isgenderlituncay2004@gmail.com>
2023-10-23 06:40:57 -0700

    a

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - prints a string, in reverse
+ * @s: checker
+ *
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+	int i = 0, j;
+	char *str = s;
+
+	while (*s != '\0')
+	{
+		s++;
+		i++;
+	}
+	for (j = i - 1 ; j <= 0 ; j--)
+	{
+		_putchar(str[j]);
+	}
+	_putchar('\n');
+}
