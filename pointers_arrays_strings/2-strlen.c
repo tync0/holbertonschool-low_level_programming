aef8292ef19937d11405047a9ca1ab93a87439f2
tync0 <isgenderlituncay2004@gmail.com>
2023-10-23 06:24:21 -0700

    a

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @s: checker
+ *
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		i++;
+	}
+	
+	return (i);
+}
