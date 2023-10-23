501a8aa0c8170f4c27f9f7d706b9d036281aaf8f
tync0 <isgenderlituncay2004@gmail.com>
2023-10-23 08:15:10 -0700

    a

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts_half - prints half of a string
+ * @str: checker
+ *
+ * Return: Always 0.
+ */
+void puts_half(char *str)
+{
+	int i = 0, j;
+	char *s = str;
+
+	while (*str != '\0')
+	{
+		str++;
+		i++;
+	}
+	for (j = i / 2 ; j < i ; j++)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
