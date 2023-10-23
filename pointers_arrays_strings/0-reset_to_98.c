198af771a3988f2ac0ccd8a93312ecb4059c71c8
tync0 <isgenderlituncay2004@gmail.com>
2023-10-23 05:15:35 -0700

    f

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 -  takes a pointer to an int as parameter and updates the value it points to to 98 
+ *
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+	 *n = 98;
+}
