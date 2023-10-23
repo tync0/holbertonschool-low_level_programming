c849767fe10fd7704167dd390ca39d4152941f65
tync0 <isgenderlituncay2004@gmail.com>
2023-10-23 09:27:15 -0700

    a

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy -  copies the string
+ * @dest: checker
+ * @src: checker
+ *
+ * Return: Always 0.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; *src != '\0' ; i++)
+	{
+		*(dest + i) = *src;
+		src++;
+	}
+
+	return dest;
+}
