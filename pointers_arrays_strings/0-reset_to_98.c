e55b69c5b355b18045f5dd820d2eb556d1a27d99
tync0 <isgenderlituncay2004@gmail.com>
2023-10-23 05:17:07 -0700

    a

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -3,10 +3,10 @@
 
 /**
  * reset_to_98 -  takes a pointer to an int as parameter and updates the value it points to to 98 
- *
+ * @n: checker
  * Return: Always 0.
  */
 void reset_to_98(int *n)
 {
-	 *n = 98;
+	*n = 98;
 }
