574396360291fc04ef2bf66807a3a4c3755a5059
tync0 <isgenderlituncay2004@gmail.com>
2023-10-23 08:07:38 -0700

    a

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: checker
+ *
+ * Return: Always 0.
+ */
+void puts2(char *str)
+{
+	int i = 0, j;
+	char *s = str;
+
+	while (*str != '\0')
+	{
+		str++;
+		i++;
+	}
+	for (j = 0 ; j < i ; j+=2)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
