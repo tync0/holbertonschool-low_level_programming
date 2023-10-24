63e29b173b343178fd3ccb123b67261ebc0c2333
tync0 <isgenderlituncay2004@gmail.com>
2023-10-24 08:41:27 -0700

    a

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -12,12 +12,16 @@ int _strcmp(char *s1, char *s2)
 {
 	int num = 0;
 
-	while (*s1 == *s2)
+	while (*s1)
 	{
+		if (*s1 != *s2)
+		{
+			num = *s1 - *s2;
+			break;
+		}
 		s1++;
 		s2++;
 	}
-	num = *s1 - *s2;
 
 	return (num);
 }
