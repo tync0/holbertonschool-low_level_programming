227afb9649847f92686fd00d9851bcd6190ee3bc
tync0 <isgenderlituncay2004@gmail.com>
2023-10-24 06:58:23 -0700

    a

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -11,11 +11,19 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int size2 = strlen(src), j;
+	int size1 = strlen(dest), size2 = strlen(src), j;
 
 	for (j = 0 ; n >= size2 ? j <= size2 : j < n ; j++)
 	{
 		*(dest + j) = *(src + j);
+	}
+	if (size1 < size2 && n >= size2)
+	{
+		while (*(dest + j) != '\0')
+		{
+			*(dest + j) == NULL;
+			j++;
+		}
 	}
 
 	return (dest);
