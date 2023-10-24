dc304dc120de5d1a65381c182756c8358c17ca33
tync0 <isgenderlituncay2004@gmail.com>
2023-10-24 05:56:17 -0700

    a

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: checker
+ * @src: checker
+ *
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int size1 = strlen(dest), size2 = strlen(src), j;
+
+	for (j = 0 ; j <= size2 ; j++)
+	{
+		*(dest + size1 - 1 + j) = *(src + j); 
+	}
+	return dest;
+}
