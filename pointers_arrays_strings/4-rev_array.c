a42e1b5d3d0dd1435fe5c659f1029b7796eea4a2
tync0 <isgenderlituncay2004@gmail.com>
2023-10-24 08:57:05 -0700

    a

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -10,10 +10,10 @@
  */
 void reverse_array(int *a, int n)
 {
-	int size = sizeof(a) / sizeof(int), i;
+	int i;
 
-	for (i = 0 ; i < size / 2 ; i++)
+	for (i = 0 ; i < n / 2 ; i++)
 	{
-		a[i] = a[size - 1 - i];
+		a[i] = a[n - 1 - i];
 	}
 }
