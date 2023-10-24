983dbf7c86a769dc0629184401b0d753139d1cdb
tync0 <isgenderlituncay2004@gmail.com>
2023-10-24 09:09:13 -0700

    a

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @s: checker
+ *
+ * Return: Always 0.
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (*(s + i))
+	{
+		if (*(s + i) >= 97 && *(s + i) <= 122)
+			*(s + i) -= 32;
+		i++;
+	}
+
+	return (s);
+}
