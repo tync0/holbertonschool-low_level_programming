e8f7c4750a05a80f446c546d4ee12e3cfccc40b0
tync0 <isgenderlituncay2004@gmail.com>
2023-10-26 02:57:11 -0700

    a

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: checker
+ * @b: checker
+ * @n: checker
+ *
+ * Return: Always 0.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int size2 = strlen(s), j;
+
+	if (n >= size2)
+	{
+		for (j = 0 ; j < size2 ; j++)
+		{
+			*(s + j) = b;
+		}
+
+		return (s);
+	}
+	else
+	{
+		for (j = 0 ; j < n ; j++)
+			*(s + j) = b;
+
+		return (s);
+	}
+
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -20,5 +20,6 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *s);
 char *cap_string(char *s);
+char *_memset(char *s, char b, unsigned int n);
 
 #endif
