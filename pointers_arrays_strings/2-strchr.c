eef2e06bdae19b96f51d9618513f49a68e1793e0
tync0 <isgenderlituncay2004@gmail.com>
2023-10-26 03:34:54 -0700

    a

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strchr -  locates a character in a string
+ * @s: checker
+ * @c: checker
+ *
+ * Return: nothing.
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0, size = strlen(s);
+
+	while (*s)
+	{
+		if (*s == c)
+			break;
+		s++;
+		i++;
+	}
+
+	if (i == size)
+		return (NULL);
+	else
+		return (s);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -22,5 +22,6 @@ char *string_toupper(char *s);
 char *cap_string(char *s);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 
 #endif
