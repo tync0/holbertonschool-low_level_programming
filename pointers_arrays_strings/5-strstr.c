8ed3c637cbb4d37be7595789dd49bf518849f4a2
tync0 <isgenderlituncay2004@gmail.com>
2023-10-27 06:36:34 -0700

    a

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strstr -  locates a substring
+ * @haystack: checker
+ * @needle: checker
+ *
+ * Return: int.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0, size2 = strlen(haystack), j = 0, c = 0, size1 = strlen(needle);
+
+	while (*(needle + i))
+	{
+		for (; j < size2 ; j++)
+		{
+			if (*(needle + i) == haystack[j])
+			{
+				c++;
+				break;
+			}
+		}
+		i++;
+	}
+
+	if (c != size1)
+		return (NULL);
+	else
+		return (haystack + j);
+}
