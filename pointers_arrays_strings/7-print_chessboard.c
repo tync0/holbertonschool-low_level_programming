c813b15f97cd3cd7b1b9bd27a029c33ed314751c
tync0 <isgenderlituncay2004@gmail.com>
2023-10-27 07:16:22 -0700

    a

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -14,9 +14,9 @@ void print_chessboard(char (*a)[8])
 	{
 		for (j = 0 ; j < 8 ; j++)
 		{
-			_putchar(a[i]);
-			_putchar(a[j]);
+			_putchar(a[i][j]);
 		}
+		_putchar('\n');
 	}
 	_putchar('\n');
 }
