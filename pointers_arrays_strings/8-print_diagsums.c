627ca86129966c9bd64081775967e10b053feecc
tync0 <isgenderlituncay2004@gmail.com>
2023-10-28 11:22:21 -0700

    a

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,24 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagsums - prints the sum of the two diagonal
+ * @a: checker
+ * @size: checker
+ **/
+void print_diagsums(int *a, int size)
+{
+	int i, sum1 = 0, sum2 = 0;
+
+	for (i = 0 ; i < size ; i++)
+	{
+		sum1 += a[i][i];
+	}
+	for (i = size - 1 ; i >= 0 ; i--)
+	{
+		sum2 += a[i][i];
+	}
+	printf("%d, ", sum1);
+	printf("%d\n", sum2);
+}
