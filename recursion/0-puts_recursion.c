94e9f6d0583f8e69348a936b7ec7dfbc13423a88
tync0 <isgenderlituncay2004@gmail.com>
2023-10-30 05:11:52 -0700

    a

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -12,6 +12,10 @@ void _puts_recursion(char *s)
 		_putchar('\n');
 		return;
 	}
-	_putchar(*s);
-	_puts_recursion(*(s + 1));
+	else
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
+	}
 }
--- recursion/main.h
+++ recursion/main.h
@@ -2,4 +2,5 @@
 #define MAIN_H
 
 int _putchar(char c);
+void _puts_recursion(char *s);
 #endif
