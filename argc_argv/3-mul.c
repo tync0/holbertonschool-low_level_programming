c35892192cef25e3c1fe5c7df4f093ef7df3a813
tync0 <isgenderlituncay2004@gmail.com>
2023-11-05 09:24:03 -0800

    sas

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - check the code
+ *
+ * @argc: var
+ * @argv: var
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int result, num1, num2;
+
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
+	printf("%d\n", result);
+	return (0);
+}
