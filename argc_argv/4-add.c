844674c71129fd29d4819494f43c6a62e7894561
tync0 <isgenderlituncay2004@gmail.com>
2023-11-05 09:46:10 -0800

    ds

---
+++ argc_argv/4-add.c
@@ -0,0 +1,55 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * check_num - check the code
+ *
+ * @s: var
+ *
+ * Return: Always 0.
+ */
+int check_num(char *s)
+{
+	int count;
+
+	count = 0;
+	while (count < strlen(s))
+	{
+		if(!isdigit(s[count]))
+			return (0);
+		count++;
+	}
+	return (1);
+}
+
+
+/**
+ * main - check the code
+ *
+ * @argc: var
+ * @argv: var
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	for (i = 1; i < argc ; i++)
+	{
+		if (check_num(argv[i]))
+		{
+			sum += atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
