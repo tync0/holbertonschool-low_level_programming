9fc881531f855e45b81832b34a236268fa137719
tync0 <isgenderlituncay2004@gmail.com>
2023-11-05 04:58:18 -0800

    ff

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * @argc: var
+ * @argv: var
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
