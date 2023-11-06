9c43731ca2c76e7cf93411883099b857ce238a31
tync0 <isgenderlituncay2004@gmail.com>
2023-11-06 03:47:55 -0800

    a

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -12,16 +12,19 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	if (size == 0)
-		return (NULL);
-	char *s;
+	char *s = malloc(sizeof(char) * size);;
 	unsigned int i;
 
-	*s = malloc(size * sizeof(char));
+	if (size == 0)
+		return (NULL);
+	if (s == NULL)
+		return (NULL):
 
 	for (i = 0 ; i < size ; i++)
 	{
-		*(s + i) = c;
+		s[i] = c;
 	}
+	s[i] = '\0';
+
 	return (s);
 }
