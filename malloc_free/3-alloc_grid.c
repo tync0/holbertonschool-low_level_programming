b01dbd8813e9353f565d9807b13fba5f229aa757
tync0 <isgenderlituncay2004@gmail.com>
2023-11-06 05:32:47 -0800

    a

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -17,7 +17,7 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	a = malloc(width * height * sizeof(int));
+	a = malloc(height * sizeof(int *));
 	for (i = 0 ; i < width ; i++)
 	{
 		for (j = 0 ; j < height ; j++)
