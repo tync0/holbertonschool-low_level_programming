d3de08eedb343bf3d7ee2c7415d420170f41c008
tync0 <isgenderlituncay2004@gmail.com>
2023-11-06 06:12:33 -0800

    a

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -29,9 +29,9 @@ int **alloc_grid(int width, int height)
 			return (NULL);
 		}
 	}
-	for (i = 0 ; i < width ; i++)
+	for (i = 0 ; i < height ; i++)
 	{
-		for (j = 0 ; j < height ; j++)
+		for (j = 0 ; j < width ; j++)
 			a[i][j] = 0;
 	}
 
