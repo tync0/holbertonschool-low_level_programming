f92ba3fee6b10ed0333a698447ef19f686107b59
tync0 <isgenderlituncay2004@gmail.com>
2023-11-08 05:06:20 -0800

    a

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,15 @@
+nclude "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: var
+ * @s2: var
+ * @n: var
+ *
+ * Return: Always 0.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{}
--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -2,4 +2,5 @@
 #define MAIN_H
 
 int _putchar(char c);
+void *malloc_checked(unsigned int b);
 #endif
