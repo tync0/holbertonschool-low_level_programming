a860200ef4ba0b2d8fc42664c8a4a3e8e4f8a3b2
tync0 <isgenderlituncay2004@gmail.com>
2023-11-08 02:19:58 -0800

    sasa

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ *
+ * @b: var
+ *
+ * Return: Always 0.
+ */
+void *malloc_checked(unsigned int b)
+{
+}
