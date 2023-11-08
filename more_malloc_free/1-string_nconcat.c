7fbae9c5bdd5e23c046678067a61e347226488d3
tync0 <isgenderlituncay2004@gmail.com>
2023-11-08 06:27:05 -0800

    a

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -16,10 +16,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	int size1, size2, i;
 	char *ptr;
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
-	size1 = strlen(s1);
-	size2 = n >= strlen(s2) ? strlen(s2) : n;
+	size1 = s1 != NULL ? strlen(s1) : 0;
+	size2 = s2 != NULL ? (n >= strlen(s2) ? strlen(s2) : n) : 0;
 	ptr = malloc(size1 + size2 + 1);
 	if (ptr == NULL)
 		return (NULL);
@@ -28,6 +26,5 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0 ; i < size2 ; i++)
 		ptr[size1 + i] = s2[i];
 	ptr[size1 + size2] = '\0';
-	
 	return (ptr);
 }
