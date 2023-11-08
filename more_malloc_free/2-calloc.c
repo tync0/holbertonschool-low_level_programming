74b9372e397dd54fbf64e41d0a52af8318dba274
tync0 <isgenderlituncay2004@gmail.com>
2023-11-08 06:39:38 -0800

    a

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -13,11 +13,14 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *p;
+	int i = 0;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	p = malloc(nmemb * size);
 	if (p == NULL)
 		return (NULL);
+	for (i = 0 ; i < nmemb ; i++)
+		p[i] = 0;
 	return (p);
 }
