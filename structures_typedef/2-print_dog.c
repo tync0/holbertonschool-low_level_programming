692347d846e9d5c1c3fa53faedaa0ddccefed656
tync0 <isgenderlituncay2004@gmail.com>
2023-11-20 04:28:54 -0800

    a

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -10,6 +10,6 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 	d->name == NULL ? printf("Name: (nil)\n") : printf("Name: %s\n", d->name);
-	d->age == NULL ? printf("Age: (nil)\n") : printf("Age: %f\n", d->age);
-	d->owner == NULL ? printf("Owner: (nil)\n") : printf("Owner: %f\n", d->owner);
+	d->age == NULL ? printf("Age: 0.\n") : printf("Age: %f\n", d->age);
+	d->owner == NULL ? printf("Owner: (nil)\n") : printf("Owner: %s\n", d->owner);
 }
