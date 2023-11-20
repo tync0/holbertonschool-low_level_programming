5b8fff61cc62537f51d62522c74092e354084836
tync0 <isgenderlituncay2004@gmail.com>
2023-11-20 04:16:22 -0800

    a

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * init_dog - initialize valuse to dog
+ * @d: dog structure
+ * @name: name of dog
+ * @age: age of th dog
+ * @owner: owner of the dog
+ * Return: none
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}
