f6b55799f0a366d8da331b97e6ae99ce5ea80590
tync0 <isgenderlituncay2004@gmail.com>
2023-11-21 04:22:47 -0800

    a

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include "dog.h"
+#include <stdlib.h>
+#include <string.h>
 /**
  * new_dog - create a new dog
  * @name: value
@@ -9,10 +11,36 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int nm, ow, i;
 	dog_t *dog;
-	dog->name = name;
+	
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+	nm = strlen(name);
+	ow = strlen(owner);
+	dog->name = malloc(nm + 1);
+	if (dog->name == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	for (i = 0 ; i < nm ; i++)
+		dog->name[i] = name[i];
+	dog->name[i] = '\0';
 	dog->age = age;
-	dog->owner = owner;
+	dog->owner = malloc(ow + 1);
+	if (dog->owner == NULL)
+	{
+		free(dog->owner);
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	for (i = 0 ; i < ow ; i++)
+		dog->owner[i] = owner[i];
+	dog->owner[i] = '\0';
 	
 	return (dog);
 }
