f844e6797557134c08c2ac61d32e876758f97121
tync0 <isgenderlituncay2004@gmail.com>
2023-11-21 04:32:10 -0800

    a

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - check the code
+ *
+ * @d: var
+ *
+ * Return: Always 0.
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->name);
+	free(d->owner);
+}
