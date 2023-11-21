f78af7e4f3ec05ba08eea48ddc7b9ca39dc450c5
tync0 <isgenderlituncay2004@gmail.com>
2023-11-21 04:09:58 -0800

    a

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -9,10 +9,10 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t dog;
-	dog.name = name;
-	dog.age = age;
-	dog.owner = owner;
+	dog_t *dog;
+	dog->name = name;
+	dog->age = age;
+	dog->owner = owner;
 	
 	return (dog);
 }
