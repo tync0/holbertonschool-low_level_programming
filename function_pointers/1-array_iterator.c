1c481a7ff4866aa07017a8f383c8bb5cc1be07be
tync0 <isgenderlituncay2004@gmail.com>
2023-11-23 05:18:22 -0800

    a

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - prints an integer
+ * @array: the integer to print
+ * @size: var
+ * @action: var
+ *
+ * Return: Nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (action == NULL)
+		return;
+	for (i = 0 ; i < size ; i++)
+		action(array[i]);
+}
