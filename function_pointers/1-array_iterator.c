f6d66c8301e656cc73c0ba569e74b869bd9be439
tync0 <isgenderlituncay2004@gmail.com>
2023-11-23 05:20:57 -0800

    a

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -16,6 +16,8 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	if (action == NULL)
 		return;
+	if (array == NULL)
+		return;
 	for (i = 0 ; i < size ; i++)
 		action(array[i]);
 }
