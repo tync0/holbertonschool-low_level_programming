81897868d79148c9d2aee5efcd0f76cf5a8c1297
tync0 <isgenderlituncay2004@gmail.com>
2023-11-23 05:28:39 -0800

    a

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - check if a number is equal to 98
+ * @array: the integer to check
+ * @size: size
+ * @cmp: cmp
+ *
+ * Return: 0 if false, something else otherwise.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0, j;
+	if (cmp == NULL)
+		return;
+	if (array == NULL)
+		return;
+	if (size <= 0)
+		return (-1);
+	for (j = 0 ; j < size ; j++)
+		i += cmp(array[j]);
+	if (i == 0)
+		return (-1);
+	else
+		return i;
+}
