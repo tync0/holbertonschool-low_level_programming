b9b8016961710ecc1e1e6cc999f9f3fb2d2e8442
tync0 <isgenderlituncay2004@gmail.com>
2023-11-24 13:41:09 -0800

    s

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -5,7 +5,7 @@
 /**
  * print_numbers - prints numbers
  * @n: val
- * @seperator: val
+ * @separator: val
  * Return: Always 0.
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
@@ -18,10 +18,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	for (i = 0 ; i < n ; i++)
 	{
-		if (seperator != NULL)
-			printf("%s%d", *seperator, va_arg(ptr, int));
+		if (separator != NULL)
+			printf("%s%d", *separator, va_arg(ptr, int));
 		else
-			printf("%d", va_arg(ptr, int);
+			printf("%d", va_arg(ptr, int));
 	}
 	printf("\n");
 	va_end(ptr);
