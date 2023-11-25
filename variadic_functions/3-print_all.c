1861edb9619194f8d66cc784874c1e50a0c14ad0
tync0 <isgenderlituncay2004@gmail.com>
2023-11-25 02:36:54 -0800

    f

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -34,9 +34,11 @@ void print_all(const char * const format, ...)
 			case 's':
 				s = va_arg(ptr, char *);
 				if (s == NULL)
+				{
 					printf("(nil)");
-				else
-					printf("%s", s);
+					break;
+				}
+				printf("%s", s);
 				break;
 			default:
 				continue;
