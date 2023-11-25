148da042488dee47658e396856fd797af4442956
tync0 <isgenderlituncay2004@gmail.com>
2023-11-25 02:08:55 -0800

    s

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -14,11 +14,13 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list ptr;
 	unsigned int i;
+	char *s;
 
 	va_start(ptr, n);
 	for (i = 0 ; i < n ; i++)
 	{
-		va_arg(ptr, char *) == NULL ? printf("(nil)") : printf("%s", va_arg(ptr, char *));
+		s = va_arg(ptr, char *);
+		s == NULL ? printf("(nil)") : printf("%s", s);
 		if (separator != NULL && i < n - 1)
 			printf("%s", separator);
 	}
