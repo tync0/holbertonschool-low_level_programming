6e5689b123ce613ee21e79265f0bd2ed8f7939c0
tync0 <isgenderlituncay2004@gmail.com>
2023-12-04 08:43:46 -0800

    a

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+  * free_list - frees a list_t list
+  * @head: start of list
+  * Return: new list
+  */
+void free_list(list_t *head)
+{
+	list_t *ptr;
+
+	while (head)
+	{
+		ptr = head;
+		head = head->next;
+		free(ptr);
+	}
+	free(head);
+}
