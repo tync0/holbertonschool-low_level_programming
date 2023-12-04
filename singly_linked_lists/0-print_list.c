fd74e28f71a76a70bb998451086d57f324f161dd
tync0 <isgenderlituncay2004@gmail.com>
2023-12-04 05:12:34 -0800

    a

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -12,19 +12,19 @@
 size_t print_list(const list_t *h)
 {
 	size_t size = 0;
-	list_t *head;
+	list_t *head = h;
 
 	while (head->next)
 	{
 		if (head->str)
-			printf("[%u] %s\n", strlen(head->str), head->str);
+			printf("[%lu] %s\n", strlen(head->str), head->str);
 		else
 			printf("[0] (nil)");
 		size++;
 		head = head->next;
 	}
 	if (head->str)
-		printf("[%u] %s\n", strlen(head->str), head->str);
+		printf("[%lu] %s\n", strlen(head->str), head->str);
 	else
 		printf("[0] (nil)");
 	return (size + 1);
