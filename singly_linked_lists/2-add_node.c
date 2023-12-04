4d9d16cd87fb32b0fdfac3d70de5d8e271e9af00
tync0 <isgenderlituncay2004@gmail.com>
2023-12-04 06:26:42 -0800

    a

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_node -  adds a new node
+ * @head: var
+ * @str: var
+ *
+ * Return: Always 0.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new->str = strdup(str);
+	head = new;
+
+	return (head);
+}
