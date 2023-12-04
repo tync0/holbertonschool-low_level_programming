27282a6f6b56a900d52067ec14de37df4dc4c5d9
tync0 <isgenderlituncay2004@gmail.com>
2023-12-04 09:01:43 -0800

    a

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * salam - prints given text before the main block
+ */
+void salam(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
