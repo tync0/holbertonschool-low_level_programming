45c828b0460e76d2ed8724302379c2683d62aef9
tync0 <isgenderlituncay2004@gmail.com>
2023-12-05 06:10:40 -0800

    a

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -11,9 +11,20 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *ptr = *h, *node = malloc(sizeof(dlistint_t));
+	dlistint_t *ptr, *node = malloc(sizeof(dlistint_t));
 	unsigned int i;
 	
+	if (!node)
+                return (NULL);
+	node->n = n;
+	node->next = NULL;
+	node->prev = NULL;
+	if (!(*h))
+	{
+		*h = node;
+		return (node);
+	}
+	ptr = *h;
 	for (i = 0 ; i < idx ; i++)
 	{
 		if (ptr)
@@ -21,9 +32,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		else
 			return (NULL);
 	}
-	if (!(*h) || !node || !ptr)
-		return (NULL);
-	node->n = n;
 	node->prev = ptr;
 	node->next = ptr->next;
 	ptr->next->prev = node;
