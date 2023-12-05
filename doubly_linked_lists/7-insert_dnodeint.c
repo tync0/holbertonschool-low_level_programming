1bf5fe74d902c208e6626d477c0a30faff82adc4
tync0 <isgenderlituncay2004@gmail.com>
2023-12-05 08:33:41 -0800

    a

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -26,7 +26,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	for (i = 0 ; i < idx ; i++)
 	{
 		ptr = ptr->next;
-		if (!ptr && i + 1 == idx - 1)
+		if (!ptr && i == idx - 1)
 			return (add_dnodeint_end(h, n));
 		else if (!ptr)
 			return (NULL);
