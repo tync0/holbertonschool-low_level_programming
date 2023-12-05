22ee255f7916616bc076395319d26f8d8153428a
tync0 <isgenderlituncay2004@gmail.com>
2023-12-05 08:12:55 -0800

    a

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -14,9 +14,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *ptr, *node = malloc(sizeof(dlistint_t));
 	unsigned int i;
 
-	if (!(*h) && idx > 0)
-		return (NULL);
 	if (!(*h) && idx == 0)
+		return (add_dnodeint(h, n));
+	else if (!*h)
+		return (NULL);
+	if (idx == 0)
 		return (add_dnodeint(h, n));
 	if (!node)
 		return (NULL);
@@ -30,9 +32,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			return (NULL);
 	}
 	node->n = n;
-	node->next = ptr;
 	node->prev = ptr;
-	ptr->prev->next = node;
-	ptr->prev = node;
+	node->next = ptr->next;
+	ptr->next->prev = node;
+	ptr->next = node;
 	return (node);
 }
