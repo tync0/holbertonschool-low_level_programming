1e8a254c2d30f15ccac40ce3c4a611c95f4a891f
tync0 <isgenderlituncay2004@gmail.com>
2023-12-05 04:20:07 -0800

    a

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a dlistint_t list
+ * @head: var
+ *
+ * Return: Always 0.
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *ptr;
+
+	while (head)
+	{
+		ptr = head;
+		head = head->next;
+		free(ptr->prev);
+		free(ptr->next);
+		free(ptr);
+	}
+	free(head);
+}
