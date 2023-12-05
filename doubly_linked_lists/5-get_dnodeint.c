287085cae3e15b6c78b9237c9627e316096115ae
tync0 <isgenderlituncay2004@gmail.com>
2023-12-05 04:32:57 -0800

    a

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -17,5 +17,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	{
 		ptr = ptr->next;
 	}
-	return ptr == NULL ? (NULL) : (ptr);
+	if (ptr)
+		return (ptr);
+	return (NULL);
 }
