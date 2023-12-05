d1ef2532efd2d5971a59590b6c855a0bea3aefa8
tync0 <isgenderlituncay2004@gmail.com>
2023-12-05 05:50:07 -0800

    a

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * insert_dnodeint_at_index - inserts a new node
+ * @head: pointer to node
+ * @index: val
+ * Return: number of nodes
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *ptr, *node = malloc(sizeof(dlistint_t));
+
+	ptr = get_dnodeint_at_index(*h, idx);
+	if (!(*h) || !ptr || !node)
+		return (NULL);
+	node->n = n;
+	node->prev = ptr;
+	ptr->next = node;
+	node->next = ptr->next->next;
+	return (node);
+}
