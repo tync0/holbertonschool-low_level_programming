812b818af6eb4907f54a190949af82caa5755e6c
tync0 <isgenderlituncay2004@gmail.com>
2023-12-08 03:12:24 -0800

    a

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -1,32 +1,17 @@
 #include <stdlib.h>
-#include <math.h>
-#include <stdio.h>
 #include "main.h"
 #include <string.h>
 /**
-  * binary_to_uint - binary to unsigned int convertor
-  * @b: binary data
+  * print_binary - prints the binary representation of a number
+  * @n: val
   * Return: unsigned integer if sucess if not 0
   */
 void print_binary(unsigned long int n)
 {
-	unsigned long int x, i;
-
-	if (n == 0)
+	while (n > 0)
 	{
-		printf("0");
-		return;
-	}
-	x = log2(n);
-	for (i = x ; i > 0 ; i--)
-	{
-		if (n >= pow(2, i))
-		{
-			printf("1");
-			n -= pow(2, i);
-		}
-		else
-			printf("0");
+		_putchar(n >> 1 + '0');
+		n = n >> 1;
 	}
 	return;
 }
