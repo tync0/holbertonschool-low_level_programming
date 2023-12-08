700de5b1422fdb8d6a703d6b09e83944b60b824d
tync0 <isgenderlituncay2004@gmail.com>
2023-12-08 05:28:12 -0800

    a

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 /**
-  * get_bit - returns the value of a bit at a given index
+  * clear_bit - returns the value of a bit at a given index
   * @n: val
   * @index: val
   * Return: unsigned integer if sucess if not 0
@@ -10,12 +10,13 @@
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int k;
+
 	if (index > 63)
 		return (-1);
 	k = (*n >> index) & 1;
 	if (index == 0)
 		*n -= 1;
 	else if (k)
-	       *n &= (0 << index);
+		*n &= (0 << index);
 	return (1);
 }
