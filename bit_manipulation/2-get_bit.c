fa9fbe218c4db1054a8bd702caf3ce9b0a9e89b1
tync0 <isgenderlituncay2004@gmail.com>
2023-12-08 04:19:02 -0800

    a

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+  * get_bit - returns the value of a bit at a given index
+  * @n: val
+  * @index: val
+  * Return: unsigned integer if sucess if not 0
+  */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	return (n >> index);
+}
