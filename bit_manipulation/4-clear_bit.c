f1eee256604ca0e9317bdb431a7afbc86ec3caf4
tync0 <isgenderlituncay2004@gmail.com>
2023-12-08 04:43:33 -0800

    a

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -9,11 +9,8 @@
   */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int k;
-
 	if (index > 63)
 		return (-1);
-	k = *n;
-	k &= (0 << index);
+	*n &= (0 << index);
 	return (1);
 }
