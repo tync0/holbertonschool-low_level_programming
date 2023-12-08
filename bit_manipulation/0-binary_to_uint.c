7436e0702cc4ea12ad8b1ef9c3a0172eb0ff593b
tync0 <isgenderlituncay2004@gmail.com>
2023-12-08 02:29:37 -0800

    h

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+  * binary_to_uint - binary to unsigned int convertor
+  * @b: binary data
+  * Return: unsigned integer if sucess if not 0
+  */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int x = 0;
+	int size = 0, i = 0;
+
+	if (!b)
+		return (0);
+	size = strlen(b);
+	while (b[i])
+	{
+		if (b[i] == '0' || b[i] == '1')
+			x += (b[i] - '0') << (size - i - 1);
+		else
+			return (0);
+	}
+	return (x);
+}	
+
