895ad4be9f56d5890043c4937c52a31f7228809f
tync0 <isgenderlituncay2004@gmail.com>
2023-12-12 06:14:44 -0800

    1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -20,17 +20,13 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_WRONLY);
+	fd = open(filename, O_WRONLY | O_CREAT | O_EXCL | O_RDWR);
 	if (fd == 3)
 	{
 		if (text_content)
 			write(STDIN_FILENO, text_content, strlen(text_content));
 		return (0);
 	}
-	fd = create(filename, O_RDONLY | O_WRONLY);
-	if (fd == -1)
+	else
 		return (-1);
-	if (text_content)
-		write(STDIN_FILENO, text_content, strlen(text_content));
-	return (0);
 }
