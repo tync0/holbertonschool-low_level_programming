ae196fcae66f5ec86737df635e78cd19f3db69f3
tync0 <isgenderlituncay2004@gmail.com>
2023-12-12 09:40:25 -0800

    a

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -6,10 +6,10 @@
 #include <string.h>
 
 /**
- * read_textfile - check the code
+ * create_file - create file
  *
  * @filename: var
- * @letters: var
+ * @text_content: var
  *
  * Return: Always 0.
  */
@@ -20,12 +20,12 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fd = open(filename,O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 	if (!text_content)
 		text_content = "";
 	write(fd, text_content, strlen(text_content));
 	close(fd);
-	return (0);
+	return (1);
 }
