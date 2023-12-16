17b825a2a046489f587b288607a9b69b38a68c74
tync0 <isgenderlituncay2004@gmail.com>
2023-12-16 08:47:59 -0800

    a

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -18,15 +18,20 @@ void hash_table_delete(hash_table_t *ht)
 		if (ht->array[l])
 		{
 			node = ht->array[l]->next;
+			free(ht->array[l]->value);
+			free(ht->array[l]->key);
 			free(ht->array[l]);
 			while (node)
 			{
 				tmp = node;
 				node = node->next;
+				free(tmp->key);
+				free(tmp->value);
 				free(tmp);
 			}
 		}
 		l++;
 	}
+	free(ht->array);
 	free(ht);
 }
