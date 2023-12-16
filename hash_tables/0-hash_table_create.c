3eabadfb072125d63dd475bcad5a476820ca338b
tync0 <isgenderlituncay2004@gmail.com>
2023-12-16 05:00:17 -0800

    a

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -8,7 +8,7 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *arr = malloc(sizeof(hash_table_t));
+	hash_table_t *arr = malloc(size * sizeof(hash_table_t));
 
 	if (!arr)
 		return (NULL);
