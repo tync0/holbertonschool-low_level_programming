ee434cecb809b814c43e9dd1461cb790f44cfb6a
tync0 <isgenderlituncay2004@gmail.com>
2023-12-16 08:57:13 -0800

    a

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -31,6 +31,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	if (strcmp(ht->array[l]->key, key) == 0)
 	{
+		free(ht->array[l]->value);
 		ht->array[l]->value = strdup(value);
 		return (1);
 	}
