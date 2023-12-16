cc3c8501c7a7032348d90e3d68ceb653925a3bb2
tync0 <isgenderlituncay2004@gmail.com>
2023-12-16 08:02:13 -0800

    a

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "hash_tables.h"
+#include <string.h>
+/**
+  * hash_table_print - retrieves a value associated with a key.
+  * @ht: hash_table
+  * Return:  actual number of letters it could read and print
+  */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int l = 0;
+	hash_node_t *node = malloc(sizeof(hash_node_t));
+
+	if (!ht || !node)
+		return;
+	printf("{");
+	while (!ht->array[l])
+	{
+		if (l != 0)
+			printf(", ");
+		node = ht->array[l];
+		printf("'%s': '%s'", node->key, node-value);
+		node = node->next;
+		while (node)
+		{
+			printf(", '%s': '%s'", node->next, node->value);
+			node = node->next;
+		}
+		l++;
+	}
+	printf("}");
+}
