671d0a4bf0d01bc514d3c2f5d453b4a22e3acff2
tync0 <isgenderlituncay2004@gmail.com>
2023-12-16 04:58:47 -0800

    a

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,16 @@
+#include "hash_tables.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the array
+ * Return: pointer to the newly created hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *arr = malloc(sizeof(hash_table_t));
+
+	if (!arr)
+		return (NULL);
+	return(arr);
+}
