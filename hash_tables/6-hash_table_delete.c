ec7f0dd9ec3e0305b6610a1a69bd600656feaf3f
tync0 <isgenderlituncay2004@gmail.com>
2023-12-16 08:36:49 -0800

    a

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include "hash_tables.h"
+#include <string.h>
+/**
+  * hash_table_delete - deletes a hash table.
+  * @ht: hash_table
+  * Return:  actual number of letters it could read and print
+  */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int l = 0;
+	hash_node_t *node, *tmp;
+
+	if (!ht)
+		return;
+	while (l < ht->size)
+	{
+		if (ht->array[l])
+		{
+			node = ht->array[l]->next;
+			free(ht->array[l]);
+			while (node)
+			{
+				tmp = node;
+				node = node->next;
+				free(tmp);
+			}
+		}
+		l++;
+	}
+	free(ht);
+}
