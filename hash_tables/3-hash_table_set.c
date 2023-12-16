6d7ac2d22b6cd7566401e241eba87165f1d34f47
tync0 <isgenderlituncay2004@gmail.com>
2023-12-16 07:08:25 -0800

    a

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -12,11 +12,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int l;
 	hash_node_t new;
-	hash_table_t *node; 
+	hash_node_t *node; 
 
 	if (!key)
 		return (0);
-	l = key_index(key, ht->size);
+	l = key_index((unsigned char)key, ht->size);
 	node = ht->array[l];
 	if (!node->key)
 	{
