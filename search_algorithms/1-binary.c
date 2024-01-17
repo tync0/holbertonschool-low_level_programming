02db1cae1691abc3df15f515c77208069211cced
tync0 <isgenderlituncay2004@gmail.com>
2024-01-17 05:34:06 -0800

    Binary Search Algorithm

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -24,7 +24,7 @@ int binary_search(int *array, size_t size, int value)
 				printf(", ");
 			printf("%d", array[i]);
 		}
-		mid = low + (high - low) / 2
+		mid = low + (high - low) / 2;
 		if (array[mid] == value)
 			return (mid);
 		if (array[mid] < value)
