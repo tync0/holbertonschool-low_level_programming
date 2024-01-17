5bf316101d9bb84dd0dabbbf73cd8802b94c008f
tync0 <isgenderlituncay2004@gmail.com>
2024-01-17 05:04:24 -0800

    linear

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -17,6 +17,7 @@ int linear_search(int *array, size_t size, int value)
 
 	for (i = 0 ; i < size ; i++)
 	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
 	}
